f788f025c9b1ae19a3b6a92c916a2f6d73cec0c5
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-11-30 19:07:17 +0800

    complete RedBlack<T>::remove()

--- DSA/redblack.h
+++ DSA/redblack.h
@@ -72,5 +72,22 @@ BinNodePosi(T) RedBlack<T>::insert(const T& e) { // 将e插入红黑树
 	solveDoubleRed(x); return x ? x : BST<T>::_hot->parent; // 经双红修正后，即可返回
 } // 无论e是否存在于原树中，返回时总有x->data == e
 
+template<typename T>
+bool RedBlack<T>::remove(const T& e) { // 从红黑树中删除关键码e
+	BinNodePosi(T)& x = search(e); if (!x) return false; // 确认目标存在（留意对_hot的设置）
+	BinNodePosi(T) r = removeAt(x, BST<T>::_hot); if (!(--BinTree<T>::_size)) return true; // 实施删除
+	// assert：_hot某一孩子刚被删除，且被r所指节点（可能是NULL）接替。以下检查是否失衡，并做必要调整
+	if (!_hot) { // 若刚被删除的根节点，则将其置黑，并更新黑高度
+		BinTree<T>::_root->color = RB_BLACK; updateHeight(_root); return true;
+	}
+	// assert：以下，原x（现r）必非根，_hot必非空
+	if (BlackHeightUpdated(*_hot)) return true; // 若所有祖先的黑深度依然平衡，则无需调整
+	if (IsRed(r)) { // 否则，若r为红，则只需令其转黑
+		r->color = RB_BLACK; r->height++; return true;
+	}
+	// assert：以下，原x（现r）均为黑色
+	solveDoubleBlack(r); return true; // 经双黑调整后返回
+} // 若目标节点存在且被删除，返回true；否则返回false
+
 _DSA_END
 #endif // !__REDBLACK_H__
