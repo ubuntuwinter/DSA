c0e5212124d69b6d00cf81ba8f04cbc15fe80c4c
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-11-30 18:01:44 +0800

    complete RedBlack<T>::updateHeight()

--- DSA/redblack.h
+++ DSA/redblack.h
@@ -23,5 +23,14 @@ public:
 	bool remove(const T& e) override; // 删除（重写）
 	// BST::search()等其余接口可直接沿用
 };
+
+template<typename T>
+int dsa::RedBlack<T>::updateHeight(BinNodePosi(T) x) { // 更新节点高度
+	x->height = max(stature(x->lc), stature(x->rc)); // 孩子一般黑高度相等，除非出现双黑
+	return IsBlack(x) ? x->height++ : x->height; // 若当前节点为黑，则计入黑深度
+} // 因统一定义stature(NULL) = -1，故height比黑高度少一，好在不致影响到各种算法中的比较判断
+
+
+
 _DSA_END
 #endif // !__REDBLACK_H__
