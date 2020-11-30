f39732691a5454024357c0b738b1e6d17df6a2f2
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-11-30 09:59:26 +0800

    change some name of var

--- DSA/bTree.h
+++ DSA/bTree.h
@@ -12,8 +12,8 @@ protected:
 	int _order; // B-树的阶次，至少为3——创建时指定，一般不能修改
 	BTNodePosi(T) _root; // 根节点
 	BTNodePosi(T) _hot; // BTree::search()最后访问的非空（除非树空）的节点位置
-	void release(BTNodePosi(T)); // 释放所有节点
-	void solveOverflow(BTNodePosi(T)); // 因插入而上溢之后的分裂处理
+	void release(BTNodePosi(T) v); // 释放所有节点
+	void solveOverflow(BTNodePosi(T) v); // 因插入而上溢之后的分裂处理
 	void solveUnderflow(BTNodePosi(T)); // 因插入而下溢之后的合并处理
 public:
 	BTree(int order = 3) :_order(order), _size(0) { // 构造函数：默认为最低的阶
@@ -32,12 +32,12 @@ public:
 }; // BTree
 
 template<typename T>
-void BTree<T>::release(BTNodePosi(T) p) {
-	if (!p) return;
-	for (int i = 0; i < p->child.size(); i++) {
-		release(p->child[i]);
+void BTree<T>::release(BTNodePosi(T) v) {
+	if (!v) return;
+	for (int i = 0; i < v->child.size(); i++) {
+		release(v->child[i]);
 	}
-	delete p;
+	delete v;
 }
 
 template<typename T>
