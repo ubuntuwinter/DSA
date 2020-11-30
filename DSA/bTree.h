0b8fede345bf1b8a991389ed08eef72c0a078dbd
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-11-30 09:55:11 +0800

    complete bTree<T>::search() && bTree<T>::insert()

--- DSA/bTree.h
+++ DSA/bTree.h
@@ -40,5 +40,27 @@ void BTree<T>::release(BTNodePosi(T) p) {
 	delete p;
 }
 
+template<typename T>
+BTNodePosi(T) BTree<T>::search(const T& e) { // 在B-树中查找关键码e
+	BTNodePosi(T) v = _root; _hot = NULL; // 从根节点出发
+	while (v) { // 逐层查找
+		Rank r = v->key.search(e); // 在当前节点中，找到不大于e的最大关键码
+		if ((0 <= r) && (e == v->key[r])) return v; // 成功：在当前节点中命中目标关键码
+		_hot = v; v = v->child[r + 1]; // 否则，转入对应子树（_hot指向其父）——需做I/O，最费时间
+	} // 这里在向量内是二分查找，但对通常的_order可直接顺序查找
+	return NULL; // 失败：最终抵达外部节点
+}
+
+template<typename T>
+bool BTree<T>::insert(const T& e) { // 将关键码e插入B树中
+	BTNodePosi(T) v = search(e); if (v) return false; // 确认目标节点不存在
+	Rank r = _hot->key.search(e); // 在节点_hot的有序关键码向量中查找合适的插入位置
+	_hot->key.insert(r + 1, e); // 将新关键码插至对应的位置
+	_hot->child.insert(r + 2, NULL); // 创建一个空子树指针
+	_size++; // 更新全树规模
+	solveOverflow(_hot); // 如有必要，需做分裂
+	return true; // 插入成功
+}
+
 _DSA_END
 #endif // !__BTREE_H__
