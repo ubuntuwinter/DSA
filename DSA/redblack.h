38063eaf452f22724bd885dfc72ba605dc49dcfa
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-11-30 20:18:26 +0800

    test RedBlack failed. 我觉得原因有以下两个可能：insert的时候高度黑更新不正确，或者是因为rotateAt函数不能处理外部节点

--- DSA/redblack.h
+++ DSA/redblack.h
@@ -120,7 +120,7 @@ bool RedBlack<T>::remove(const T& e) { // 从红黑树中删除关键码e
 	BinNodePosi(T)& x = BST<T>::search(e); if (!x) return false; // 确认目标存在（留意对_hot的设置）
 	BinNodePosi(T) r = removeAt(x, BST<T>::_hot); if (!(--BinTree<T>::_size)) return true; // 实施删除
 	// assert：_hot某一孩子刚被删除，且被r所指节点（可能是NULL）接替。以下检查是否失衡，并做必要调整
-	if (!BST<T>::_hot) { // 若刚被删除的根节点，则将其置黑，并更新黑高度
+	if (!BST<T>::_hot) { // 若刚被删除的是根节点，则将其置黑，并更新黑高度
 		BinTree<T>::_root->color = RB_BLACK; updateHeight(BinTree<T>::_root); return true;
 	}
 	// assert：以下，原x（现r）必非根，_hot必非空
--- DSA/test_redblack.cpp
+++ DSA/test_redblack.cpp
@@ -6,5 +6,31 @@ using namespace dsa;
 
 int main() {
 	RedBlack<int> rb;
+	rb.insert(70);
+	rb.insert(80); 
+	rb.insert(90);
+	rb.insert(10);
+	rb.insert(100);
+	rb.insert(60);
+	rb.insert(120);
+	rb.insert(130);
+	rb.insert(140);
+	rb.insert(150);
+	cout << "size: " << rb.size() << endl;
+	Print<int> print;
+	rb.travLevel(print);
+	cout << endl;
+	rb.travIn(print);
+	cout << endl;
+	rb.remove(100);
+	rb.remove(70);
+	rb.remove(150);
+	rb.remove(10);
+	rb.remove(60);
+	cout << "size: " << rb.size() << endl;
+	rb.travLevel(print);
+	cout << endl;
+	rb.travIn(print);
+	cout << endl;
 	return 0;
 }
