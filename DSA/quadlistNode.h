927a1308be1cae9ebee022ce5607a02847ddc27d
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-12-07 19:52:52 +0800

    complete QuadlistNode::insertAsSuccAbove()

--- DSA/quadlistNode.h
+++ DSA/quadlistNode.h
@@ -14,10 +14,19 @@ struct QuadlistNode { // QuadlistNode模板类
 	QlistNodePosi(T) above; QlistNodePosi(T) below; // 上邻、下邻
 	QuadlistNode // 构造器
 	(T e = T(), QlistNodePosi(T) p = NULL, QlistNodePosi(T) s = NULL,
-		QlistNodePosi(T) a = NULL, Ql istNodePosi(T) b = NULL)
+		QlistNodePosi(T) a = NULL, QlistNodePosi(T) b = NULL)
 		: entry(e), pred(p), succ(s), above(a), below(b) {}
 	QlistNodePosi(T) insertAsSuccAbove // 插入新节点，以当前节点为前驱，以节点b为下邻
 	(T const& e, QlistNodePosi(T) b = NULL);
 };
+
+template<typename T>
+QlistNodePosi(T) QuadlistNode<T>::insertAsSuccAbove(T const& e, QlistNodePosi(T) b) {
+	QlistNodePosi(T) x = new QuadlistNode<T>(e, this, succ, NULL, b);
+	succ->pred = x; succ = x; // 设置水平逆向连接
+	if (b) b->above = x; // 设置垂直逆向连接
+	return x; // 返回新节点位置
+}
+
 _DSA_END
 #endif // !__QUADLISTNODE_H__
