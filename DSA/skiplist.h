4288b65ddf4b74e1883936ba338091b8e6b1c285
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-12-07 20:02:49 +0800

    complete Skiplist::remove()

--- DSA/skiplist.h
+++ DSA/skiplist.h
@@ -80,5 +80,22 @@ V* dsa::Skiplist<K, V>::get(K k) { // 跳转表词条查找算法
 	return skipSearch(qlist, p, k) ? &(p->entry.value) : NULL; // 查找并报告
 } // 有多个命中时靠后者优先
 
+template<typename K, typename V>
+bool Skiplist<K, V>::remove(K k) { // 跳转表词条删除算法
+	if (empty()) return false; // 空表情况
+	ListNode<Quadlist<Entry<K, V>>*>* qlist = first(); // 从顶层Quadlist的
+	QuadlistNode<Entry<K, V>>* p = qlist->data->first(); // 首节点开始
+	if (!skipSearch(qlist, p, k)) return false; // 目标词条不存在，直接返回
+	do {
+		QuadlistNode<Entry<K, V>>* lower = p->below; // 记住下一层节点，并
+		qlist->data->remove(p); // 删除当前层节点
+		p = lower; qlist = qlist->succ; // 转入下一层
+	} while (qlist->succ); // 如上不断重复，直到塔基
+	while (!empty() && first()->data->empty()) { // 逐一地
+		List<Quadlist<Entry<K, V>>*>::remove(first()); // 清除已可能不含词条的顶层Quadlist 
+	}
+	return true; // 删除操作成功完成
+}
+
 _DSA_END
 #endif // !__SKIPLIST_H__
