0e757ed92f6ddd885790dcab87107afa7175fc0e
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-12-07 20:03:08 +0800

    complete Quadlist::remove()

--- DSA/quadlist.h
+++ DSA/quadlist.h
@@ -47,6 +47,14 @@ void dsa::Quadlist<T>::init() { // Quadlist初始化，创建Quadlist对象时统一调用
 }
 
 template<typename T>
+T Quadlist<T>::remove(QlistNodePosi(T) p) { // 删除Quadlist内位置p处的节点，返回其中存放的词条
+	// assert: p为Quadlist中的合法位置
+	p->pred->succ = p->succ; p->succ->pred = p->pred; _size--; // 摘除节点
+	T e = p->entry; delete p; // 备份词条，释放节点
+	return e;
+}
+
+template<typename T>
 QlistNodePosi(T) Quadlist<T>::insertAfterAbove(T const& e, QlistNodePosi(T) p, QlistNodePosi(T) b) { // 将e作为p的后继、b的上邻插入Quadlist
 	_size++; return p->insertAsSuccAbove(e, b); // 返回新节点位置（below = NULL）
 }
