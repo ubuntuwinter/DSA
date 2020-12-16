6849e04fcccb9a5772fba1b0480ce52026778199
刘怡豪 <liuyihao18@mails.tsinghua.edu.cn>
2020-12-16 19:13:04 +0800

    add test_string

---
+++ DSA/test_string.cpp
@@ -0,0 +1,16 @@
+// test_string.cpp
+#include "string.h"
+#include <iostream>
+using namespace std;
+using namespace dsa;
+
+int main() {
+	char T[] = "Data Structure";
+	char P[] = "Structure";
+
+	cout << "Brute Force 1: " << match_BruteForce1(P, T) << endl;
+	cout << "Brute Force 2: " << match_BruteForce2(P, T) << endl;
+	cout << "KMP: " << match_KMP(P, T) << endl;
+	
+	return 0;
+}
